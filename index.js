acd31edcf034eca1badb5cac88c576ada772a16f
KrhystFerrari <krhyst_ferrari@hotmail.com>
2025-01-23 11:56:47 -0300

    feat: add items

--- index.js
+++ index.js
@@ -25,26 +25,32 @@ app.get("/", (req, res) => {
         type: "hero",
         data: {
           sectionOne: {
-            title: "Title section one",
-            color: "#FF5733",
+            title: "SBT Sports",
+            color: "#46aabe",
             items: [
               {
-                title: "title section one item one",
-                titleColor: "#000000",
-                image: "https://placehold.co/670x380",
-                link: "https://news.com/market-update",
-                subtitle: "Subtitle section one item one",
-                subtitleColor: "#555555",
+                title:
+                  "Australian Open 2025: Brasil tem quatro representantes no primeiro Grand Slam do ano",
+                titleColor: "",
+                image:
+                  "https://sbt-sports-assets-prod.s3.sa-east-1.amazonaws.com/thumbnail_australian_open_2025_brasil_tem_quatro_representantes_no_primeiro_grand_slam_do_ano_7fdf12dd33.jpg",
+                link: "https://sports.sbt.com.br/noticia/esportes/australian-open-2025-brasil-tem-quatro-representantes-no-primeiro-grand-slam-do-ano-1",
+                subtitle:
+                  "João Fonseca, Thiago Monteiro, Thiago Wild e Bia Haddad representam o Brasil no primeiro Grand Slam do ano",
+                subtitleColor: "",
                 duration: "5 min",
                 videoLink: "https://news.com/video/market-update",
               },
               {
-                title: "title section one item two",
+                title:
+                  "Santos é goleado pelo Athletic em primeiro teste com Caixinha",
                 titleColor: "#1E90FF",
-                image: "https://placehold.co/670x380",
-                link: "https://news.com/tech",
-                subtitle: "Subtitle section one item one",
-                subtitleColor: "#666666",
+                image:
+                  "https://sbt-sports-assets-prod.s3.sa-east-1.amazonaws.com/thumbnail_santos_e_goleado_pelo_athletic_em_primeiro_teste_com_caixinha_e16182a2aa.jpg",
+                link: "https://sports.sbt.com.br/noticia/esportes/santos-e-goleado-pelo-athletic-em-primeiro-teste-com-caixinha-1",
+                subtitle:
+                  "Peixe é derrotado por 4 a 1 em preparação para o Paulistão; único gol foi marcado por Guilherme",
+                subtitleColor: "",
               },
               {
                 title: "title section one item three",
@@ -57,60 +63,106 @@ app.get("/", (req, res) => {
             ],
           },
           sectionTwo: {
-            title: "Title section two",
-            color: "#4CAF50",
+            title: "SBT News",
+            color: "#5e0bff",
             items: [
               {
-                title: "subtitle section two item one",
-                titleColor: "#FFFFFF",
-                image: "https://placehold.co/320x180",
-                link: "https://news.com/football",
-                subtitle: "Team A vs Team B",
-                subtitleColor: "#AAAAAA",
+                title:
+                  "Saiba quem são os ocupantes de avião que explodiu após atravessar pista em Ubatuba (SP)",
+                titleColor: "",
+                image:
+                  "https://sbt-news-assets-prod.s3.sa-east-1.amazonaws.com/thumbnail_familia_aviao_ubatuba_ecd30732aa.jpg",
+                link: "https://sbtnews.sbt.com.br/noticia/brasil/saiba-quem-sao-os-ocupantes-de-aviao-que-explodiu-apos-atravessar-pista-em-ubatuba-sp",
+                subtitle:
+                  "Piloto morreu, 4 ocupantes sobreviveram. Pai e crianças foram transferidos para hospital de Caraguatatuba, cidade vizinha; mãe segue internada em Ubatuba ",
+                subtitle2: "",
+                subtitleColor: "",
+                duration: "",
+                videoLink: "",
               },
             ],
           },
           sectionThree: {
-            title: "title section three",
-            color: "#9C27B0",
+            title: "Pop",
+            color: "#000000",
             items: [
               {
-                title: "subtitle section three item three",
-                titleColor: "#FFD700",
-                image: "https://placehold.co/320x180",
-                link: "https://news.com/awards",
-                subtitle: "",
-                subtitleColor: "#CCCCCC",
+                title:
+                  "Foto de Gabriel Medina nos Jogos Olímpicos ganha 'Oscar' da fotografia esportiva",
+                titleColor: "",
+                image:
+                  "https://sbt-news-assets-prod.s3.sa-east-1.amazonaws.com/thumbnail_medina_c1a8051825.png",
+                link: "https://sbtnews.sbt.com.br/noticia/mundo/foto-de-gabriel-medina-nos-jogos-olimpicos-ganha-oscar-da-fotografia-esportiva",
+                subtitle:
+                  "Registro foi feito por Jerome Brouillet no terceiro dia do surfe na Olimpíada de Paris em 2024",
+                subtitle2: "",
+                subtitleColor: "",
+                duration: "",
+                videoLink: "",
+                category: {
+                  id: "dd485e41f1758def296e1bc7377f8ea7",
+                  name: "mundo",
+                },
               },
               {
-                title: "subtitle section three item three",
-                titleColor: "#FFD700",
-                image: "https://placehold.co/320x180",
-                link: "https://news.com/awards2",
-                subtitle: "",
-                subtitleColor: "#CCCCCC",
+                title:
+                  "Ubatuba registrou seis acidentes aéreos nos últimos oito anos; relembre",
+                titleColor: "",
+                image:
+                  "https://sbt-news-assets-prod.s3.sa-east-1.amazonaws.com/thumbnail_Ubatuba_registrou_seis_acidentes_aereos_nos_ultimos_tres_anos_Montagem_Bombeiros_e_Reproducao_2191e79fce.png",
+                link: "https://sbtnews.sbt.com.br/noticia/brasil/ubatuba-registrou-seis-ocorrencias-aereas-nos-ultimos-oito-anos-relembre",
+                subtitle:
+                  "Três dos acidentes, incluindo o desta quinta-feira (9), tiveram vítimas fatais; levantamento foi feito a partir de dados do Cenipa",
+                subtitle2: "",
+                subtitleColor: "",
+                duration: "",
+                videoLink: "",
+                category: {
+                  id: "6a4120be23c814f80233ecbb34e71adc",
+                  name: "brasil",
+                },
               },
             ],
           },
           sectionFour: {
-            title: "title section four",
-            color: "#FF9800",
+            title: "Programas de TV",
+            color: "#000000",
             items: [
               {
-                title: "subtitle section four item four",
-                titleColor: "#FFFFFF",
-                image: "https://placehold.co/320x180",
-                link: "https://news.com/wellness",
-                subtitle: "Stay fit in 2024",
-                subtitleColor: "#333333",
+                title:
+                  "Envenenamento no Piauí: Perícia investiga cajus que podem ter intoxicado parentes de família morta com arroz",
+                titleColor: "",
+                image:
+                  "https://sbt-news-assets-prod.s3.sa-east-1.amazonaws.com/thumbnail_imagem_site_640x440_2_1_fec39b65eb.png",
+                link: "https://sbtnews.sbt.com.br/noticia/brasil/envenenamento-no-piaui-pericia-investiga-cajus-que-podem-ter-envenenado-parentes-de-familia-morta-por-arroz",
+                subtitle:
+                  "O IML do Piauí informou que realiza exames nos cajus supostamente envenenados",
+                subtitle2: "",
+                subtitleColor: "",
+                duration: "",
+                videoLink: "",
+                category: {
+                  id: "6a4120be23c814f80233ecbb34e71adc",
+                  name: "brasil",
+                },
               },
               {
-                title: "subtitle section four item four",
-                titleColor: "#FFFFFF",
-                image: "https://placehold.co/320x180",
-                link: "https://news.com/wellness2",
-                subtitle: "Stay fit in 2024",
-                subtitleColor: "#333333",
+                title:
+                  'Lula critica decisão da Meta de encerrar checagem de fatos e convoca reunião: "Extremamente grave"',
+                titleColor: "",
+                image:
+                  "https://sbt-news-assets-prod.s3.sa-east-1.amazonaws.com/thumbnail_54223051440_0fe2b64627_o_b9f62fb513.jpg",
+                link: "https://sbtnews.sbt.com.br/noticia/politica/lula-critica-decisao-da-meta-de-encerrar-checagem-de-fatos-e-convoca-reuniao-extremamente-grave",
+                subtitle:
+                  '"Queremos que cada país tenha sua soberania resguardada. Não pode um cidadão, dois, três, achar que pode ferir a soberania de uma nação", comentou',
+                subtitle2: "",
+                subtitleColor: "",
+                duration: "",
+                videoLink: "",
+                category: {
+                  id: "d20f58eeb146b2c530634c225a4bf799",
+                  name: "politica",
+                },
               },
             ],
           },
